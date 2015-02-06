ea507d2ba3ab636767206e5b7280616d639ad096
Nikki Chumakov <nikkikom@gmail.com>
2015-02-06 18:44:31 +0300

    on_request.h now includes evrthg from on_request/*

---
+++ on_request.h
@@ -0,0 +1,8 @@
+#ifndef _HTTP_SERVER_ON_REQUEST_H_
+#define _HTTP_SERVER_ON_REQUEST_H_
+
+#include <http_server/on_request/callback.h>
+#include <http_server/on_request/coroutine.h>
+#include <http_server/on_request/uri_parsed.h>
+
+#endif // _HTTP_SERVER_ON_REQUEST_H_
