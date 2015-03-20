fe5daca91369fab05bf3afc5ae7f859c62ba23e7
Nikki Chumakov <nikkikom@gmail.com>
2015-03-20 20:19:45 +0300

    Wrong namespace name for is_yield_context

--- on_request/uri_parsed.h
+++ on_request/uri_parsed.h
@@ -237,7 +237,7 @@ private:
 template <class Error, class Iterator, class SmartSock, class Handler>
 detail::on_request_uri_parsed<Iterator, Handler>
 on_request (Handler&& handler, typename boost::enable_if_c<
-    ! boost::is_yield_context<Error>::value &&
+    ! detail::is_yield_context<Error>::value &&
     boost::is_same<typename boost::result_of<
       typename boost::decay<Handler>::type (
             Error, http::HttpMethod, http::url,
